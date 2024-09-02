090b7af6499372aa5da7ea03e8d2539e2f4128ba
shivsalunke0503 <shivsalunke.s@gmail.com>
2024-09-02 22:53:05 +0530

    adding pointer programs

---
+++ Application Prorams/pointer1.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    char ch ='A';
+    int i =10;
+    float f = 30.4f;
+    double d = 90.12345;
+
+    char *chpointer= &ch;
+    printf("Address of ch = %u \n",&ch);
+    printf("chpointer holds the address of ch and address is %u \n",chpointer);
+    printf("chpointer fetch the value from addrss which is stored in it %c \n",*chpointer);
+    
+    int *iPointer = &i;
+    float *fPointer = &f;
+    double *dPointer= &d;
+    printf("the size of iPonter is %d \n",sizeof(*iPointer));
+
+
+
+
+
+
+
+
+    
+    return 0;
+}
